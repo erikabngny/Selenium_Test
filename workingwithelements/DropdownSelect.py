0a2b79718f9cba9b0299ca1f6072374bf94d1abe
erika-bonganay <erika.bonganay@codeandtheory>
2019-10-08 14:29:34 +0800

    Working with Dropdown Select

---
+++ workingwithelements/DropdownSelect.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.select import Select
+
+
+class DropdownSelect():
+
+    def test(self):
+        baseURL = "https://learn.letskodeit.com/p/practice"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseURL)
+        driver.implicitly_wait(10)
+
+
+        element = driver.find_element_by_id("carselect")
+        sel = Select(element)
+
+        sel.select_by_value("benz")
+        print("Select Benz by value")
+        time.sleep(2)
+
+        sel.select_by_index("2")
+        print("Select Honda by index")
+        time.sleep(2)
+
+        sel.select_by_visible_text("BMW")
+        print("Select BMW by visible text")
+        time.sleep(2)
+
+        sel.select_by_index(2)
+        print("Select Honda by visible index")
+
+        
+
+
+
+
+ff = DropdownSelect()
+ff.test()
